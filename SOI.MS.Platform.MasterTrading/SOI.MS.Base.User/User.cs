83b8f33e17bada1a991184290e5ec6c0d6e8f89f
AldoRobert <aldorobert2005@yahoo.es>
2025-03-12 17:34:55 -0400

    Base.User and WebAPI, WebAPI2

---
+++ SOI.MS.Platform.MasterTrading/SOI.MS.Base.User/User.cs
@@ -0,0 +1,25 @@
+﻿namespace SOI.MS.Base.User
+{
+    public class User
+    {
+       
+        #region  "  Constructor  "
+        public User()
+        {
+
+        }
+        #endregion
+
+
+        #region "  Public Properties  "
+       
+        public int iduser { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; }      
+        #endregion
+    }
+}
